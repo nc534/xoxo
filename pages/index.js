b9e16ff4c26d872c0c3bb3969b0b277310ca1ea2
sssssabinaaa <sabina@withcomet.com>
2023-02-09 17:59:05 -0500

    more favicon stuff

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Comet React SDK</title>
+        <title>❤️❤️❤️</title>
         <meta
           name="description"
           content="The Comet React SDK lets you launch new Solana NFT collections in your app!"
